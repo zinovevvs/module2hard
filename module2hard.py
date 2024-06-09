9e134f6656463d0ca9b61b24ab3250c6de32af45
zinovevvs <ziryanov97@gmail.com>
2024-06-09 17:47:07 +0500

    Update module2hard.py

--- module2hard.py
+++ module2hard.py
@@ -1,23 +1,15 @@
-def shifr(k, j):
-    list_ = [3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20]
-    key1 = []
-    key2 = []
-    import random
-    for i in range(3, 21):
-        i = random.choice(list_[i])
-    for (k, j) in range(1, i):
-        k = True
-        j = True
-    if 1 <= k <= i and 1 <= j <= i:
-        k = True
-        j = True
-    elif k + j <= i:
-        k = True
-        j = True
-    elif (k + j) % i == 0:
-        k = True
-        j = True
-        key1.append(key1)
-        key2.append(key2)
 
-        print(k, j)
+n = int(input('Введите число от 3 до 20:'))
+
+
+def find_password(n):
+    result = list()
+    for i in range(1, n):
+        for j in range(i + 1, n):
+            if n % (i + j) == 0:
+                result.append(str(i) + str(j))
+    return result
+
+
+get_result = find_password(n)
+print(str(n) + ' - ' + ''.join([str(x) for x in get_result]))
